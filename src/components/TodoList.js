d3b279258d9982b3f83a1fbd1a3bf86340e58073
unknown <serjumano17@gmail.com>
2020-08-24 23:53:17 -0300

    refactoring to use react hooks

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import '../w3.css'
 
-class TodoList extends React.Component {
-    render() {
-        return (
-            <ul class="w3-ul">
-                {this.props.items.map(item => (
-                    <li key={item.id}>{item.text}</li>
-                ))}
-            </ul>
-        );
-    }
+const TodoList = ({ items }) => {
+    return (
+        <ul class="w3-ul">
+            {items.map(item => (
+                <li key={item.id}>{item.text}</li>
+            ))}
+        </ul>
+    )
 }
 
-export default TodoList;
+export default TodoList
